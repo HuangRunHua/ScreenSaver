fb6e2d43da6b29445ef2466278d56fbc89844a4a
Huang Runhua <45262463+HuangRunHua@users.noreply.github.com>
2019-09-12 14:55:57 +0800

    Initial Commit

---
+++ ScreenSaver/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScreenSaver
+//
+//  Created by Runhua Huang on 2019/9/12.
+//  Copyright © 2019 Runhua Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
